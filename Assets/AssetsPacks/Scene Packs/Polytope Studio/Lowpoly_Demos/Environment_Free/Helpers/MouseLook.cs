22b3a4df00af662c47484ff096ed454ad98fe98f
RomanBesson <1350648257@qq.com>
2024-04-22 15:26:54 +0800

    2024_04_22 15:26 增加Packages文件夹

--- Assets/AssetsPacks/Scene Packs/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/MouseLook.cs
+++ Assets/AssetsPacks/Scene Packs/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/MouseLook.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    //灵敏度
     public float mouseSensitivity = 100f;
-
+    //依附的对象
     public Transform playerBody;
 
     private float xRotation = 0f;
@@ -18,11 +19,17 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //鼠标在水平方向上的移动量
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //鼠标在垂直方向上的移动量
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //视角进行相应的旋转
         xRotation -= mouseY;
+        //限制视角移动角度，防止出现极端情况
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //控制水平方向旋转
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        //让绑定的对象旋转
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
